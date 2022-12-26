5ae4bbe61092239e120a69619b04e3b0a2383ce8
Jin Wei <seetohjinwei@gmail.com>
2022-12-26 19:52:26 +0800

    Add templates and page builder

---
+++ page/page.go
@@ -0,0 +1,59 @@
+// Helped out a bunch: https://betterprogramming.pub/how-to-generate-html-with-golang-templates-5fad0d91252
+
+package page
+
+import (
+	"bufio"
+	"bytes"
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/seetohjinwei/linker/url"
+)
+
+// Generate the page's content.
+func Generate(templateDirectory string, mainTemplate string, templateNames []string, links []url.Url) []byte {
+	templatePaths := make([]string, len(templateNames))
+
+	for i, t := range templateNames {
+		path, err := filepath.Abs(templateDirectory + "/" + t)
+		if err != nil {
+			log.Fatalf("error while parsing template %q: %v", t, err)
+		}
+		templatePaths[i] = path
+	}
+
+	templates, err := template.New("").ParseFiles(templatePaths...)
+	if err != nil {
+		log.Fatalf("error while parsing template: %v", err)
+	}
+
+	filteredLinks := url.RemoveDuplicates(links)
+
+	var processed bytes.Buffer
+	err = templates.ExecuteTemplate(&processed, mainTemplate, filteredLinks)
+	if err != nil {
+		log.Fatalf("error while executing template: %v", err)
+	}
+
+	return processed.Bytes()
+}
+
+func Build(content []byte, filePath string) {
+	os.Remove(filePath)
+	f, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalf("error while creating page file: %v", err)
+	}
+	w := bufio.NewWriter(f)
+	w.Write(content)
+	w.Flush()
+}
+
+func BuildAndGenerate(links []url.Url, filePath string) {
+	content := Generate("templates", "page", []string{"page.tmpl", "header.tmpl", "content.tmpl", "footer.tmpl"}, links)
+
+	Build(content, filePath)
+}
---
+++ page/page_test.go
@@ -0,0 +1,30 @@
+package page
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/seetohjinwei/linker/url"
+)
+
+func TestGenerate(t *testing.T) {
+	names := []string{"test.tmpl"}
+	links := []url.Url{
+		{Short: "a1", Full: "full_a"},
+		{Short: "a2", Full: "full_a"},
+		{Short: "c1", Full: "full_c"},
+	}
+
+	got := Generate("../templates", "test", names, links)
+	want := []byte(`<div>
+    <ul>
+        <li><a href=full_a>a1</a></li>
+        <li><a href=full_c>c1</a></li>
+    </ul>
+</div>
+`)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %q want %q", string(got), string(want))
+	}
+}
